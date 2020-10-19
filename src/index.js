810ee58bcd5241f1e7e7849460d2645dadb846b5
nicolas.petitot <nicolas.petitot@developpement-durable.gouv.fr>
2020-10-19 14:09:22 +0200

    feat: ajoute un Dockerfile pour builder le projet et installer les modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// const moment = require("moment");
+const moment = require("moment");
 
 console.log("Version old-school : fichier html brut + script .js");
 console.log(moment().startOf("day").fromNow());
