89cd1cfd8edc4e715af703b111692a3e7447e6b2
nicolas.petitot <nicolas.petitot@developpement-durable.gouv.fr>
2020-10-22 12:00:05 +0200

    feat: configure webpack

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 const moment = require("moment");
-// import moment from "moment"
 
-console.log("Version old-school : fichier html brut + script .js");
 console.log(moment().startOf("day").fromNow());
+
+// Tester html-webpack-plugin : js -> html
+const h1 = document.createElement("h1");
+h1.textContent = "nicolaspetitot.com";
+
+const h2 = document.createElement("h2");
+h2.textContent = "VPS expérimental";
+
+const p = document.createElement("p");
+p.textContent =
+  "VPS à usage de formation sur le projet Camino de la Fabrique Numérique";
+
+const app = document.querySelector("#root");
+app.append(h1);
+app.append(h2);
+app.append(p);
+
+// Tester du code ancien
+// créer une instance de classe sans constructeur
+class StartUp {
+  name = "camino";
+}
+
+const myStartUp = new StartUp();
+
+const p2 = document.createElement("p");
+p2.textContent = `${myStartUp.name} est top !`;
+
+app.append(p2);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -15,5 +16,21 @@ module.exports = {
       template: path.resolve(__dirname, "./template.html"),
       filename: "index.html",
     }),
+    new CleanWebpackPlugin(),
   ],
+  module: {
+    rules: [
+      // JavaScript
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
